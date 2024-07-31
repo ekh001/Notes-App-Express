659afe60a3938b13ee72f1dbed25a3d1c95ac530
Liz Hall <lizk.hall@gmail.com>
2024-07-31 15:05:15 -0500

    added SignUpModal.tsx file to components

---
+++ frontend/src/components/SignUpModal.tsx
@@ -0,0 +1,16 @@
+import { User } from "../models/user"
+
+
+
+interface SignUpModalProps {
+    onDismiss: () => void,
+    onSignUpSuccessful: (user: User) => void,
+}
+
+const SignUpModal = ({onDismiss, onSignUpSuccessful}: SignUpModalProps) => {
+    return (
+
+    );
+}
+
+export default SignUpModal
