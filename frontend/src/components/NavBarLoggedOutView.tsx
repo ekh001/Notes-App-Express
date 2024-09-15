4a5d0d2276bb6057714d5290421eb702cb28c6fa
Liz Hall <lizk.hall@gmail.com>
2024-09-15 15:59:56 -0500

    adds navbar login/sign up and responsiveness

--- frontend/src/components/NavBarLoggedOutView.tsx
+++ frontend/src/components/NavBarLoggedOutView.tsx
@@ -1,10 +1,18 @@
+import { Button } from "react-bootstrap";
+
 interface NavBarLoggedOutViewProps {
     onSignUpClicked: () => void,
     onLoginClicked: () => void,
 }
 
-const NavBarLoggedOutView = () => {
-    return (  );
+const NavBarLoggedOutView = ({onSignUpClicked, onLoginClicked}: NavBarLoggedOutViewProps) => {
+    return ( 
+        <>
+            <Button onClick={onSignUpClicked}>Sign Up</Button>
+            <Button onClick={onLoginClicked}>Log In</Button>
+        
+        </>
+     );
 }
  
 export default NavBarLoggedOutView;
